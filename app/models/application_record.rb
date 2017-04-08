1178761dd40563e198b90e9c0c4e70fc1f1bb5bf
S-Hiroaki <h.shimohira@buysell-technologies.com>
2017-04-08 12:18:33 +0900

    import_csv

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # require File.expand_path('../boot', __FILE__)
+
+  require 'rails/all'
+  require 'csv'
 end
---
+++ app/models/product.rb
@@ -0,0 +1,17 @@
+class Product < ApplicationRecord
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+      product = find_by(id: row["id"]) || new
+      # CSVからデータを取得し、設定する
+      product.attributes = row.to_hash.slice(*updatable_attributes)
+      # 保存する
+      product.save!
+    end
+  end
+
+  # 更新を許可するカラムを定義
+  def self.updatable_attributes
+    ["name", "price", "released_on"]
+  end
+end
